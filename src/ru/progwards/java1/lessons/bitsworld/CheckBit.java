05074c378198bf595d31b9915e9309a3a97a736e
sidnet <sidnet@mail.ru>
2020-01-10 14:53:40 +0300

    H7 Home work

--- src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
+++ src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
@@ -2,14 +2,14 @@ package ru.progwards.java1.lessons.bitsworld;
 
 public class CheckBit {
     public static void main(String[] args) {
-        System.out.println(checkBit((byte) 0b0101_0101, 4));
+        System.out.println(checkBit((byte) 0b0101_0101, 6));
 //        System.out.println(String.format("%8s", Integer.toBinaryString(value)).replace(' ', '0'));
     }
     public static int checkBit(byte value, int bitNumber){
         final byte TEST = (byte) 0b0000_0001;
         byte result = 0;     // результат
-        for (byte i = 0; i < bitNumber; i++)
-            value >>= 1;
+//        for (byte i = 0; i < bitNumber; i++)
+            value >>= bitNumber;
         result = (byte) (value & TEST);
         return result;
     }
