c65fc74c6d2eb651bc83946fcf2927d5e5a1677d
sidnet <sidnet@mail.ru>
2020-01-31 04:05:31 +0300

    In3 Home work

--- src/ru/progwards/java1/SeaBattle/sidnet/SeaBattleAlg.java
+++ src/ru/progwards/java1/SeaBattle/sidnet/SeaBattleAlg.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class SeaBattleAlg {
     final int FIRE_FREE = -2;       //  пустое поле вокруг цели
     final int FIRE_MISS = -1;       //  пустое поле после проверки
-    final int FIRE_DEF = 0;         //  поле не проверялось
+    public final int FIRE_DEF = 0;         //  поле не проверялось
     final int FIRE_HIT = 1;         //  цель ранена
     final int FIRE_DEST = 2;        //  цель уничтожена
     final int FIRE_UNDEF = -9;      //  непонятный ответ
@@ -19,16 +19,15 @@ public class SeaBattleAlg {
     private SeaBattle seaBattle;
     private int sizeX;
     private int sizeY;
-    private int[][] field_my;
+    public int[][] field_my;
     private int palubK;         //  количество палуб в текушем раунде
     private int orient;         //  ориентация палуб в текушем раунде 1-гориз, 2-верт
     private int nosX;          //  координаты верхней левой палубы
     private int nosY;          //  координаты верхней левой палубы
     private int[] ship;
-    private static int counter;        // счетчик выстрелов
+    int counter;        // счетчик выстрелов
     private static int direct = 1;     //  направление движения
     private static int hits = 0;
-
 
     //  конструктор основного класса
     SeaBattleAlg(SeaBattle seaBattle, int sizeX, int sizeY) {
@@ -40,11 +39,7 @@ public class SeaBattleAlg {
     }
     //  методы основного класса --------------------------------------
     //  сделать выстрел и обработать результат
-    int fireXY(int x2, int y2){
-//        if (x2 < 0 || x2 >= sizeX || y2 < 0 || y2 >= sizeY ){
-//            System.out.println(seaBattle);
-//            System.out.println(this);
-//            System.out.println("Выход в точке - " + x2 + " : " + y2 + " , направление = " + direct);}
+    public int fireXY(int x2, int y2){
         SeaBattle.FireResult fireResult = seaBattle.fire(x2, y2);
         switch (fireResult) {
             case MISS:      field_my[x2][y2] = FIRE_MISS;   break;
@@ -59,10 +54,6 @@ public class SeaBattleAlg {
     boolean analiz(int f1, int x1, int y1) {
         int fire2, x2, y2;
         boolean graniza = false;
-//        if (x1 == 9 && y1 == 9){
-//            System.out.println(seaBattle);
-//            System.out.println(this);
-//            System.out.println("Выход в точке - " + x1 + " : " + y1 + " , ориентация = " + orient);}
 
         switch (f1) {
 
@@ -87,14 +78,16 @@ public class SeaBattleAlg {
                             }
                         }
                     case ORIENT_V:  //  начинаем поиск других палуб, вычисляем направление
-                        //  по вертикали
-                        x2 = x1;
-                        y2 = y1 + 1;
-                        fire2 = fireXY(x2, y2);   // двигаемся вниз
-                        if (fire2 > FIRE_DEF){
-                            this.orient = 2;    //  вертикал
-                            analiz(fire2, x2, y2);
-                            break;
+                        graniza = y1 < this.sizeY - 1;
+                        if (graniza) {          // если достигли границы по вертикали
+                            x2 = x1;
+                            y2 = y1 + 1;
+                            fire2 = fireXY(x2, y2);   // двигаемся вниз
+                            if (fire2 > FIRE_DEF){
+                                this.orient = 2;    //  вертикал
+                                analiz(fire2, x2, y2);
+                                break;
+                            }
                         }
                 }
                 break;
@@ -105,14 +98,10 @@ public class SeaBattleAlg {
                     this.nosX = x1;
                     this.nosY = y1;
                 }
-                this.palubK ++; //  может быть несколько раненных целей
-//                System.out.println("Потоп - " + this.nosX + " : " + this.nosY + " , палуб всего = "+ this.palubK + " # " + this.orient);
-                obvodka();  //  необходимо обвести корабль по периметру
+                this.palubK ++;
+                obvodka(this.nosX, this.nosY, this.palubK, this.orient);  //  необходимо обвести корабль по периметру
                 this.palubK = 0;
                 this.orient = 0;
-//  вывод для отладки
-//                System.out.println(seaBattle);
-//                System.out.println(this);
                 break;
             default:
                 System.out.println("Что-то непонятное в - " + x1 + " : " + y1);
@@ -121,20 +110,20 @@ public class SeaBattleAlg {
     }
 
     //  -----------------------------------------------------------------
-    void obvodka(){     //  необходимо обвести корабль по периметру
+    void obvodka(int nosX1, int nosY1, int pKor, int oKor){     //  необходимо обвести корабль по периметру
         int i1 = 0; int i2 = 0; int j1 = 0; int j2 = 0;
-        switch (this.orient){
+        switch (oKor){
             case 0: //  однопалубный
-                i1 = Math.max(0, this.nosX - 1);    i2 = Math.min(this.nosX + this.palubK + 1, this.sizeX);
-                j1 = Math.max(0, this.nosY - 1);    j2 = Math.min(this.nosY + this.palubK + 1, this.sizeY);
+                i1 = Math.max(0, nosX1 - 1);    i2 = Math.min(nosX1 + pKor + 1, this.sizeX);
+                j1 = Math.max(0, nosY1 - 1);    j2 = Math.min(nosY1 + pKor + 1, this.sizeY);
                 break;
             case 1: //  горизонтальный
-                i1 = Math.max(0, this.nosX - 1);    i2 = Math.min(this.nosX + 1 + this.palubK, this.sizeX);
-                j1 = Math.max(0, this.nosY - 1);    j2 = Math.min(this.nosY + 1 + 1, this.sizeY);
+                i1 = Math.max(0, nosX1 - 1);    i2 = Math.min(nosX1 + 1 + pKor, this.sizeX);
+                j1 = Math.max(0, nosY1 - 1);    j2 = Math.min(nosY1 + 1 + 1, this.sizeY);
                 break;
             case 2: //  вертикальный
-                i1 = Math.max(0, this.nosX - 1);    i2 = Math.min(this.nosX + 1 + 1, this.sizeX);
-                j1 = Math.max(0, this.nosY - 1);    j2 = Math.min(this.nosY + 1 + this.palubK , this.sizeY);
+                i1 = Math.max(0, nosX1 - 1);    i2 = Math.min(nosX1 + 1 + 1, this.sizeX);
+                j1 = Math.max(0, nosY1 - 1);    j2 = Math.min(nosY1 + 1 + pKor , this.sizeY);
                 break;
             }
         for (int i = i1; i < i2; i++)
@@ -154,8 +143,74 @@ public class SeaBattleAlg {
             analiz(fire1, x, y);
     }
 
+    int check_field(int x, int y){
+        //  проверить поле, для пустого - сделать выстрел
+//        System.out.println("Что-то непонятное в - " + x + " : " + y);
+        if (field_my[x][y] == FIRE_DEF)
+            return fireXY(x, y);
+        else
+            return field_my[x][y];
+    }
+
     //  -----------------------------------------------------------------
     public void battleAlgorithm(SeaBattle seaBattle) {
+
+//  ==========================================================
+        int check, palubKor;
+        //  пройтись по диагонали сверху направо
+        for (int y = 0; y < sizeY; y++) {       //  sizeX
+            for (int x = 0; x < sizeX; x++) {   //  sizeY
+//                if (x == y || x == y - 2 || x == y + 2 || x == y - 4 || x == y + 4 || x == y - 6 || x == y + 6 || x == y - 8 || x == y + 8)   //   || x == y - 2 || x == y + 2
+                if (x == y || x == y - 2 || x == y + 2 || x == y - 4 || x == y + 4 || x == y - 6 || x == y + 6)     //   || x == y - 8 || x == y + 8
+                    if (field_my[x][y] == 0){
+                        check = check_field(x, y);
+                        switch (check) {
+                            case FIRE_DEST:
+                                obvodka(x, y, 1, 0);   //  выполнить обводку
+                                break;
+                            case FIRE_HIT:
+                                Ship clipper = new Ship(x, y);
+                                palubKor = clipper.Build(x, y);
+                                clipper.Dost(x, y);
+//                                System.out.println(palubKor + " палуб в - " + x + " : " + y);
+                                break;
+                            default:
+                        }
+                    }
+            }
+        }
+//  ==========================================================
+        //  пройтись по диагонали сверху направо
+        for (int y = 0; y < sizeY; y++) {       //  sizeX
+            for (int x = 0; x < sizeX; x++) {   //  sizeY
+                if (x == sizeY - y - 1 || x == sizeY - y - 3 || x == sizeY - y + 1)
+                    if (field_my[x][y] == 0){
+                        check = check_field(x, y);
+                        switch (check) {
+                            case FIRE_DEST:
+                                obvodka(x, y, 1, 0);   //  выполнить обводку
+                                break;
+                            case FIRE_HIT:
+                                Ship clipper = new Ship(x, y);
+                                palubKor = clipper.Build(x, y);
+                                clipper.Dost(x, y);
+//                                System.out.println(palubKor + " палуб в - " + x + " : " + y);
+                                break;
+                            default:
+                        }
+                    }
+            }
+        }
+
+//  ==========================================================
+
+//  вывод для отладки
+//        System.out.println(seaBattle);
+//        System.out.println(this);
+//  ==========================================================
+
+
+//  ==========================================================
         for (int y = 0; y < sizeY; y++) {       //  sizeX
             for (int x = 0; x < sizeX; x++) {   //  sizeY
                 switch (direct){
@@ -170,48 +225,158 @@ public class SeaBattleAlg {
                         break;
                     default:
                 }
-                if (hits >= 20)
+                if (hits >= 20){
+                    hits = 0;
                     return;
+                }
             }
 //            direct = - direct;  //  можно отключить
         }
+//  ==========================================================
     }
 
-//    public String toString() {
-////        assert this.sizeX > 0 && this.sizeY > 0;
-//
-//        StringBuilder result = new StringBuilder(" ");
-//
-//        int y;
-//        for(y = 0; y < this.sizeX; ++y) {
-//            result.append("-").append(y % 10);
-//        }
-//
-////        result.append("-   координата x");
-//        result.append("\n");
-//
-//        for(y = 0; y < this.sizeY; ++y) {
-//            result.append(y).append("|");
-//
-//            for(int x = 0; x < this.sizeX; ++x) {
-//                result.append(this.field_my[x][y] == 0 ? " "
-//                        :(this.field_my[x][y] == -2 ? "~"
-//                        :(this.field_my[x][y] == -1 ? "-"
-//                        :(this.field_my[x][y] == 1 ? "+"
-//                        :(this.field_my[x][y] == 2 ? "X"
-//                        : "?"))))).append("|");
-//            }
-//
-//            result.append("\n");
-//        }
-//
-//        return result.toString();
-//    }
+    public String toString() {
+//        assert this.sizeX > 0 && this.sizeY > 0;
+
+        StringBuilder result = new StringBuilder(" ");
+
+        int y;
+        for(y = 0; y < this.sizeX; ++y) {
+            result.append("-").append(y % 10);
+        }
+
+//        result.append("-   координата x");
+        result.append("\n");
+
+        for(y = 0; y < this.sizeY; ++y) {
+            result.append(y).append("|");
+
+            for(int x = 0; x < this.sizeX; ++x) {
+                result.append(this.field_my[x][y] == 0 ? " "
+                        :(this.field_my[x][y] == -2 ? "~"
+                        :(this.field_my[x][y] == -1 ? "-"
+                        :(this.field_my[x][y] == 1 ? "+"
+                        :(this.field_my[x][y] == 2 ? "X"
+                        : "?"))))).append("|");
+            }
+
+            result.append("\n");
+        }
+
+        return result.toString();
+    }
+
+//  -----------------------------------------------------------------
+class Ship {
+    //  класс - корабль 1-многопалубный создается при первом попадаии
+    //  содержит методы для построения полных координат и обводки (?)
+    int aX, aY;  //  якорь, первое значение
+    int nX, nY;  //  нос, крайнее левое/верхнее значение (для обводки)
+    int orient;  //  ориентация палуб в объекте = 1-гориз, 2-верт
+    private int palubK;         //  количество палуб в текушем раунде
+
+    Ship(int aX, int aY) {
+        this.aX = aX;
+        this.aY = aY;
+        this.nX = aX;       //  фиксируем для начала строительства
+        this.nY = aY;       //
+        this.palubK = 1;    //
+    }
+
+    void Dost(int x1, int y1) {   //  достройка корабля
+        obvodka(nX, nY, palubK, orient);
+    }
+
+    int Build(int x1, int y1) {   //  достройка корабля
+        int fire2, x2, y2, check;
+        boolean graniza = false;
+
+//  ................................
+        orient = 1;
+        //  сначала двигаемся вправо
+        for (int i = 1; ; i++) {
+            graniza = x1 - i >= 0;
+            if (graniza) {          // если достигли границы
+                x2 = x1 - i;        //  точка слева
+                y2 = y1;
+                check = check_field(x2, y2);
+                if (check == FIRE_DEST) {
+                    nX = x2;        // обновление координат
+                    palubK++;
+                    return palubK;
+                } else if (check == FIRE_HIT) {
+                    palubK++;
+                    nX = x2;        // обновление координат
+                    orient = 1;     //  горизонт
+                } else
+                    break;
+            } else break;
+        }
+//  ................................
+        for (int i = 1; ; i++) {
+            graniza = x1 + i <= sizeX - 1;
+            if (graniza) {          // если достигли границы
+                x2 = x1 + i;        //  точка справа
+                y2 = y1;
+                check = check_field(x2, y2);
+                if (check == FIRE_DEST) {
+                    palubK++;
+                    return palubK;
+                } else if (check == FIRE_HIT) {
+                    palubK++;
+                    orient = 1;    //  горизонт
+                } else
+                    break;
+            } else break;
+        }
+//  ................................
+        orient = 2;
+        //  сначала двигаемся вверх
+        for (int i = 1; ; i++) {
+            graniza = y1 - i >= 0;
+            if (graniza) {          // если достигли границы
+                x2 = x1;            //  точка сверху
+                y2 = y1 - i;
+                check = check_field(x2, y2);
+                if (check == FIRE_DEST) {
+                    nY = y2;        // обновление координат
+                    palubK++;
+                    return palubK;
+                } else if (check == FIRE_HIT) {
+                    nY = y2;        // обновление координат
+                    palubK++;
+                    orient = 2;
+                } else
+                    break;
+            } else break;
+        }
+//  ................................
+        for (int i = 1; ; i++) {
+            graniza = y1 + i <= sizeY - 1;
+            if (graniza) {          // если достигли границы
+                x2 = x1;            //  точка снизу
+                y2 = y1 + i;
+                check = check_field(x2, y2);
+                if (check == FIRE_DEST) {
+                    palubK++;
+                    return palubK;
+                } else if (check == FIRE_HIT) {
+                    palubK++;
+                    orient = 2;
+                } else
+                    break;
+            } else break;
+        }
+//  ................................
+        return 0;
+    }
+}
+//  -----------------------------------------------------------------
 
     // функция для отладки
     public static void main(String[] args) {
 //        System.out.println("Sea battle");
-//        SeaBattle seaBattle = new SeaBattle();
+//        SeaBattle seaBattle = new SeaBattle(true);
 //        SeaBattleAlg my_game = new SeaBattleAlg(seaBattle, 10, 10);
 //        my_game.battleAlgorithm(seaBattle);
 //        System.out.println("Результат = " + seaBattle.getResult());
@@ -224,14 +389,16 @@ public class SeaBattleAlg {
     }
     static void TestFull() {
         System.out.println("Sea battle");
-        double result = 0;
+        double result = 0, fullCounter = 0;
         for (int i=0; i<1000; i++) {
+//            System.out.println("Счетчик цикла = " + i);
             SeaBattle seaBattle = new SeaBattle();
             SeaBattleAlg my_game = new SeaBattleAlg(seaBattle, 10, 10);
             my_game.battleAlgorithm(seaBattle);
             result += seaBattle.getResult();
-            hits = 0;   //  обнулить
+            fullCounter += my_game.counter;
         }
         System.out.println("Результат = " + result / 1000);
+//        System.out.println("Счетчик = " +  fullCounter);
     }
 }
