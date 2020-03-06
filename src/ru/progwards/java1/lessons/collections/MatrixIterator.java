536b29407e3ddfeac35c85682e02de28f13e2d28
sidnet <sidnet@mail.ru>
2020-03-07 01:21:17 +0300

    H12 Home work

--- src/ru/progwards/java1/lessons/collections/MatrixIterator.java
+++ src/ru/progwards/java1/lessons/collections/MatrixIterator.java
@@ -4,18 +4,23 @@ import java.util.Iterator;
 public class MatrixIterator<T> implements Iterator<T>{
     private T[][] array;
     private int ind;
-    private int x;  //  количество строк
-    private int y;  //  количество столбцов
+    private int x;  //  текущая строка
+    private int y;  //  текущий столбец
     private int z;  //  общеее количество
 
     MatrixIterator(T[][] array) {
         this.array = array;
-        x = array.length;
-        if (x > 0)
-            y = array[0].length;    //  предполагаем, что есть хоть одна строка;
-        else
-            y = 0;
-        z = x * y;
+        x = 0;
+        y = -1;
+        z = 0;
+        for (int i=0; i<array.length; i++)
+            for (int j=0; j<array[i].length; j++)
+                z++;
+//        if (x > 0)
+//            y = array[0].length;    //  предполагаем, что есть хоть одна строка;
+//        else
+//            y = 0;
+//        z = x * y;
         ind = -1;
     }
 
@@ -31,32 +36,39 @@ public class MatrixIterator<T> implements Iterator<T>{
     @Override
     public T next() {
         // TODO Auto-generated method stub
+        int d1 = 0;
+        int d2 = 0;
         ind++;
         try {
-            int d1 = ind / y;
-            int d2 = ind - (d1 * y);
-            return array[d1][d2];
+            if (y < array[x].length-1) {
+//                d1 = x;
+//                d2 = y;
+                y++;
+            }
+            else{
+                x++;
+                y = 0;
+            }
+//            int d1 = ind / y;
+//            int d2 = ind - (d1 * y);
+//            return array[d1][d2];
+            return array[x][y];
         } catch (ArrayIndexOutOfBoundsException e) {
             throw e;
         }
     }
 
     public static void main(String[] args) {
-        String[][] names = {{"Александр", "Анна", "Борис"}, {"Бэлла", "Василий", "Василиса"}};
+        String[][] names = {{"Александр"}, {"Анна", "Борис"}, {"Бэлла"}, {"Василий", "Василиса", "Виктор"}};
 //        String[] names = {"Александр", "Борис"};
         MatrixIterator<String> a_n = new MatrixIterator<String>(names);
-        if  (a_n.hasNext())
-            System.out.println(a_n.next());
-        if  (a_n.hasNext())
-            System.out.println(a_n.next());
-//        System.out.println(a_n.next());
-//        System.out.println(a_n.next());
-        if  (a_n.hasNext())
-            System.out.println(a_n.next());
-        if  (a_n.hasNext())
-            System.out.println(a_n.next());
-        if  (a_n.hasNext())
-            System.out.println(a_n.next());
+        if  (a_n.hasNext())            System.out.println(a_n.next());
+        if  (a_n.hasNext())            System.out.println(a_n.next());
+        if  (a_n.hasNext())            System.out.println(a_n.next());
+        if  (a_n.hasNext())            System.out.println(a_n.next());
+        if  (a_n.hasNext())            System.out.println(a_n.next());
+        if  (a_n.hasNext())            System.out.println(a_n.next());
+        if  (a_n.hasNext())            System.out.println(a_n.next());
     }
 }
 //  {"Александр","Анна"}, {"Борис", "Бэлла"}, {"Василий", "Василиса"}
