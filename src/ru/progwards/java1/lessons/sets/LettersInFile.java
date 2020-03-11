de707c71e4d2494ee55b3fc2e5ae8eb50a45ba0e
sidnet <sidnet@mail.ru>
2020-03-11 20:14:13 +0300

    H13 Home work

--- src/ru/progwards/java1/lessons/sets/LettersInFile.java
+++ src/ru/progwards/java1/lessons/sets/LettersInFile.java
@@ -6,19 +6,18 @@ import java.util.TreeSet;
 
 public class LettersInFile {
     public static String process(String fileName) throws IOException {
-        String ret = "";
-        TreeSet<Character> set1 = new TreeSet<>();  // множество для собирания символов
+        String ret = "";    //  пустая строка для результата
+        TreeSet<Character> set1 = new TreeSet<>();  // множество для символов с автоматической сортировкой
         try (RandomAccessFile raf = new RandomAccessFile(fileName, "rw")) {
             String currentLine = raf.readLine();
             if (currentLine != null){
                 currentLine = new String(currentLine.getBytes("ISO-8859-1"), "UTF-8");
-//                System.out.println(currentLine);
             }
-            char[] alphabet = currentLine.toCharArray();
-            for (char letter : alphabet){
-                if (Character.isLetter(letter))
-                    set1.add(letter);   //  добавить символ в коллекцию
-            }
+            char[] alphabet = currentLine.toCharArray();    //  строку в массив символов
+            for (char letter : alphabet)                    //  цикл по массиву символов
+                if (Character.isLetter(letter))             //  проверить, что это буква
+                    if (! set1.contains(letter))            //  проверить наличие в множестве
+                        set1.add(letter);   //  добавить символ в коллекцию если такой отсутствует
             Character[] array = set1.toArray(new Character[0]);
             for (Character letter : array)
                 ret += letter + "";
@@ -32,6 +31,5 @@ public class LettersInFile {
     public static void main(String[] args) throws IOException {
         System.out.println(process("C:\\Users\\sidne\\IdeaProjects\\HelloWorld\\src\\ru\\progwards\\java1\\lessons\\sets\\file1.txt"));
 //        System.out.println(process(null));
-
     }
 }
