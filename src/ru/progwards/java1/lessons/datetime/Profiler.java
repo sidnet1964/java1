894e85ea4323b9aa9880309fd147e2751c2c628a
sidnet1964 <sidnet@mail.ru>
2020-04-06 02:46:19 +0300

    H16 Домашнее задание

--- src/ru/progwards/java1/lessons/datetime/Profiler.java
+++ src/ru/progwards/java1/lessons/datetime/Profiler.java
@@ -1,40 +1,52 @@
 package ru.progwards.java1.lessons.datetime;
 
 import java.time.Instant;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
+import java.util.*;
 
 public class Profiler {
-//    Profiler(){
-//        stack = new ArrayDeque<>();
-//    }
-    public static void enterSection(String name){
-        Instant instant = Instant.now();
+    public Deque<Long> stack;
+    public List<StatisticInfo> info;
+    public Map<String, Long> section;   //  дополнительно для оперативной работы
 
+    Profiler(){     //  конструктор
+        stack = new ArrayDeque<>();
+        info = new ArrayList<>();
+        section = new TreeMap<>();
     }
-    public static void exitSection(String name){
-
+//  войти в профилировочную секцию, замерить время входа
+    public void enterSection(String name) throws InterruptedException {
+        System.out.println("enterSection ++++++ -> " + name);
+        Long instantIn = Instant.now().toEpochMilli();
+        stack.push(instantIn);
+        Thread.sleep(500);
     }
-    public static List<StatisticInfo> getStatisticInfo(){
-        List<StatisticInfo> info = new ArrayList<>();
-
+//   выйти из профилировочной секции. Замерить время выхода, вычислить
+//   промежуток времени между входом и выходом в миллисекундах.
+    public void exitSection(String name){
+        System.out.println("exitSection ------ -> " + name);
+        Long instantOut = Instant.now().toEpochMilli();
+        Long instantIn = stack.peek();
+//        info.add(new StatisticInfo());
+//        section.put(name, instantOut - instantIn);
+        Long prev = section.get(name);  //  предыдущее значение времени выполнения
+        System.out.println("section.size() = " + section.size());
+        if (prev == null)
+            section.put(name, instantOut - instantIn);
+        else
+            section.replace(name, prev + instantOut - instantIn);   //  обновить значение
+    }
+//  получить профилировочную статистику, отсортировать по наименованию секции
+    public List<StatisticInfo> getStatisticInfo(){
+//        List<StatisticInfo> info = new ArrayList<>();
         return info;
     }
-    class StatisticInfo{
-        public String sectionName;  //  имя секции
-        public int fullTime;        //  полное время выполнения секции в миллисекундах.
-        public int selfTime;        //  чистое время выполнения секции в миллисекундах.
-        public int count;           //  количество вызовов. В случае, если вызовов более одного ...
-    }
 
-    public static void main(String[] args) {
-        final int M_COUNT = 1_000;
-        Deque<Long> stack = new ArrayDeque<>();    //  для вложенных секций
+    public static void main(String[] args) throws InterruptedException {
+        final int M_COUNT = 10;
+//        Deque<Long> stack = new ArrayDeque<>();    //  для вложенных секций
 
         Profiler t1 = new Profiler();
-        enterSection("1");
+//        enterSection("1");
         t1.enterSection("1");
         for(int i = 0; i < M_COUNT; i++) {
             t1.enterSection("2");
@@ -45,5 +57,6 @@ public class Profiler {
         t1.enterSection("4");
         t1.exitSection("4");
         t1.exitSection("1");
+        System.out.println(t1.section);
     }
 }
---
+++ src/ru/progwards/java1/lessons/datetime/StatisticInfo.java
@@ -0,0 +1,11 @@
+package ru.progwards.java1.lessons.datetime;
+
+class StatisticInfo{
+    public String sectionName;  //  имя секции
+    public int fullTime;        //  полное время выполнения секции в миллисекундах.
+    public int selfTime;        //  чистое время выполнения секции в миллисекундах.
+    public int count;           //  количество вызовов. В случае, если вызовов более одного ...
+    StatisticInfo(){
+
+    }
+}
