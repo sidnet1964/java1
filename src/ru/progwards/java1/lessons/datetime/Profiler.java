a8a961e1fd458c97efba4be65297870bbe83eae9
sidnet1964 <sidnet@mail.ru>
2020-04-09 02:14:35 +0300

    H16 Домашнее задание

--- src/ru/progwards/java1/lessons/datetime/Profiler.java
+++ src/ru/progwards/java1/lessons/datetime/Profiler.java
@@ -9,7 +9,7 @@ public class Profiler {
     public static Map<String, Statistic> sectionStat;   //  TreeMap для замеров по секциям
 
     Profiler(){     //  конструктор +++++++++++++++++++++++++++++++++++
-        info = new ArrayList<>();
+//        info = new ArrayList<>();
         stackString = new ArrayDeque<>();
         sectionStat = new TreeMap<>();
     }
@@ -35,6 +35,11 @@ public class Profiler {
                 throw e;
             }
         }
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 //  ---------------------------------------------------------------------
 //   выйти из профилировочной секции. Замерить время выхода, вычислить
@@ -47,25 +52,23 @@ public class Profiler {
             System.out.println("exit ------- -> " + name + " | " + instantOut);
             try {
 
-            if (!stackString.isEmpty()) {
-                Statistic stat1 = sectionStat.get(name);    //  взять объект из TreeMap
-                String name2 = stackString.pop();  //  взять объект из стека, можно сравнить с name
-                System.out.println("1 stat1 - " + stat1);
-                stat1.update(instantOut);   //  расчитать время выполнения
-                sectionStat.replace(name, stat1);   //  обновить значение ArrayDeque
-                System.out.println("2 stat1 - " + stat1);
-////////////////    попробовать здесь вычесть чистое время вышестоящей сессии
                 if (!stackString.isEmpty()) {
-                    String name0 = stackString.peek();          //  прочитать объект из стека (без удаления)
-                    Statistic stat0 = sectionStat.get(name0);   //  взять объект из TreeMap
-                    System.out.println("3 stat0 - " + stat0);
-//                System.out.println("3 stat1 - " + stat1);
-                    stat0.under(stat1);
-                    System.out.println("4 stat0 - " + stat0);
-                    sectionStat.replace(stat0.sectionName, stat0);   //  обновить значение ArrayDeque
-//                System.out.println("5 stat0 - " + stat0);
+                    Statistic stat1 = sectionStat.get(name);    //  взять объект из TreeMap
+                    String name2 = stackString.pop();  //  взять объект из стека, можно сравнить с name
+                    System.out.println("1 stat1 - " + stat1);
+                    stat1.update(instantOut);   //  расчитать время выполнения
+                    sectionStat.replace(name, stat1);   //  обновить значение ArrayDeque
+                    System.out.println("2 stat1 - " + stat1);
+////////////////    попробовать здесь вычесть чистое время вышестоящей сессии
+                    if (!stackString.isEmpty()) {
+                        String name0 = stackString.peek();          //  прочитать объект из стека (без удаления)
+                        Statistic stat0 = sectionStat.get(name0);   //  взять объект из TreeMap
+                        System.out.println("3 stat0 - " + stat0);
+                        stat0.under(stat1);
+                        System.out.println("4 stat0 - " + stat0);
+                        sectionStat.replace(stat0.sectionName, stat0);   //  обновить значение ArrayDeque
+                    }
                 }
-            }
             }
             catch (NullPointerException e) {
                 throw e;
@@ -76,31 +79,42 @@ public class Profiler {
 //  ---------------------------------------------------------------------
 //  получить профилировочную статистику, отсортировать по наименованию секции
     public static List<StatisticInfo> getStatisticInfo(){
-//        List<StatisticInfo> info = new ArrayList<>();
+        Collection<Statistic> collection = sectionStat.values();
+        info = new ArrayList<>();
+        for(Statistic value : collection)
+            info.add(new StatisticInfo(value.sectionName,
+                    value.fullTime,
+                    value.fullTime-value.subrTime,
+                    value.count));
+
+//  компаратор для статистики
+        Comparator<StatisticInfo> comparator = new Comparator<StatisticInfo>() {
+            @Override
+            public int compare(StatisticInfo info1, StatisticInfo info2) {
+                return info1.sectionName.compareTo(info2.sectionName);
+            }
+        };
+        info.sort(comparator);   //  отсортировать по наименованию секции
         return info;
     }
 
+    //  =======================================================================
     public static void main(String[] args) {
         final int M_COUNT = 2;
         Profiler t1 = new Profiler();
-        enterSection("");
-//        for(int i = 0; i < M_COUNT; i++) {
-//            try {
-//                Thread.sleep(50);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//
-//            t1.enterSection("2");
-//            t1.enterSection("3");
-//            t1.exitSection("3");
-//            t1.exitSection("2");
-//        }
+
         enterSection("4");
-        exitSection("4");
+        for(int i = 0; i < M_COUNT; i++) {
+
+            enterSection("2");
+            enterSection("3");
+            exitSection("3");
+            exitSection("2");
+        }
+        enterSection("1");
         exitSection("1");
-        System.out.println(t1.sectionStat);
-//        System.out.println(t1.stackString);
-//        System.out.println(t1.stackStat);
+        exitSection("4");
+//        System.out.println(sectionStat);
+        System.out.println(getStatisticInfo());
     }
 }
--- src/ru/progwards/java1/lessons/datetime/StatisticInfo.java
+++ src/ru/progwards/java1/lessons/datetime/StatisticInfo.java
@@ -5,7 +5,20 @@ class StatisticInfo{
     public int fullTime;        //  полное время выполнения секции в миллисекундах.
     public int selfTime;        //  чистое время выполнения секции в миллисекундах.
     public int count;           //  количество вызовов. В случае, если вызовов более одного ...
-    StatisticInfo(){
 
+    public StatisticInfo(String sectionName, int fullTime, int selfTime, int count) {
+        this.sectionName = sectionName;
+        this.fullTime = fullTime;
+        this.selfTime = selfTime;
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return  "sectionName='" + sectionName + '\'' +
+                ", fullTime=" + fullTime +
+                ", selfTime=" + selfTime +
+                ", count=" + count +
+                '}';
     }
 }
