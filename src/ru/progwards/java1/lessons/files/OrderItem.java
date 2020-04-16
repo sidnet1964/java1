ca7f73b334757276fd2668fac7ca1e367fd1a718
sidnet1964 <sidnet@mail.ru>
2020-04-16 20:15:50 +0300

    H17 Домашнее задание

--- src/ru/progwards/java1/lessons/files/OrderItem.java
+++ src/ru/progwards/java1/lessons/files/OrderItem.java
@@ -1,13 +1,17 @@
 package ru.progwards.java1.lessons.files;
 
 public class OrderItem {
-    public String googsName;   // - наименование товара
-    public int count;   // - количество
-    public double price;   // - цена за единицу
+    public String googsName;    // - наименование товара
+    public int count;           // - количество
+    public double price;        // - цена за единицу
+
+    public OrderItem() {
+    }
 
     public OrderItem(String googsName, int count, double price) {   //  конструктор
         this.googsName = googsName;
         this.count = count;
         this.price = price;
     }
+
 }
