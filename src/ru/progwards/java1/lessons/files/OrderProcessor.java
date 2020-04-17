a0bcdaa9606f237155aefddd9b13c27b6e13e126
sidnet1964 <sidnet@mail.ru>
2020-04-17 20:33:24 +0300

    H17 Домашнее задание

--- src/ru/progwards/java1/lessons/files/OrderProcessor.java
+++ src/ru/progwards/java1/lessons/files/OrderProcessor.java
@@ -1,5 +1,6 @@
 package ru.progwards.java1.lessons.files;
 
+//import java.io.IOException;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -19,7 +20,7 @@ public class OrderProcessor {
     }
 
     //  загружает заказы за указанный диапазон дат, с start до finish, обе даты включительно
-    public int loadOrders(LocalDate start, LocalDate finish, String shopId) throws IOException {
+    public int loadOrders(LocalDate start, LocalDate finish, String shopId) {
         //  проверить все три параметра //  shopId (111, 112, 113, 117), имя 111-222222-3333
         int count = 0;  //  количество ошибок
         String pattern;
@@ -29,59 +30,61 @@ public class OrderProcessor {
             pattern = "glob:**/" + shopId + "-??????-????.csv";
         List<Path> fList = createList(this.startPath, pattern);    //  получить список всех файлов по шаблону
 //        System.out.println(fList);
-        Iterator<Path> it1 = fList.iterator();
-        while (it1.hasNext()) {
-            Path file = it1.next();    //  файл для загрузки
+        //  файл для загрузки
+        for (Path file : fList) {
             String name = file.getFileName().toString();    //  имя файла
-            String shop = name.substring(0,3);
-            String orde = name.substring(4,10);
-            String cust = name.substring(11,15);
-            String last = Files.getAttribute(file, "basic:lastModifiedTime").toString();
+            String shop = name.substring(0, 3);
+            String orde = name.substring(4, 10);
+            String cust = name.substring(11, 15);
+            String last = null;
+            try {
+                last = Files.getAttribute(file, "basic:lastModifiedTime").toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             //  2020-04-12T14:41:06
             //  0123456789012345678
             LocalDate lastDate = checkLastDate(last.substring(0, 10));
-            LocalDateTime lastTime = checkLastTime(last.substring(0, 10)+" "+last.substring(11, 19));
+            LocalDateTime lastTime = checkLastTime(last.substring(0, 10) + " " + last.substring(11, 19));
             if (checkOrders(start, finish, lastDate)) {
                 sumOrder = 0d;   //  сумма товаров по текущему заказу
                 //  загружаем текущий файл
                 List<OrderItem> oList = orderItem(file);
-                if (oList == null){
+                if (oList == null) {
                     count++;
                     continue;
                 }
                 listOrder.add(new Order(shop, orde, cust, lastTime, oList, sumOrder));
             }
         }
-//        System.out.println("listOrder.size() = " + listOrder.size());
-//        System.out.println(listOrder);
         return count;
     }
     //  -----------------------------------
     //  обработка содержимого одного файла
-    public List<OrderItem> orderItem(Path file) throws IOException {
+    public List<OrderItem> orderItem(Path file) {
         String googsName = "";
         int count = 0;
         double price= 0d;
         List<OrderItem> oList = new ArrayList<>();
-        List<String> allLines = Files.readAllLines(file);
+        List<String> allLines = null;
+        try {
+            allLines = Files.readAllLines(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         Iterator<String> it1 = allLines.iterator();
         boolean good = true;
         while (it1.hasNext()) {
             //  разобрать строку на части
             try(Scanner scanner = new Scanner(it1.next()).useDelimiter("\\s*,\\s*")) {
-//                while (scanner.hasNext()) {
-//                if (scanner.hasNext())
-                    googsName = scanner.next();
-//                if (scanner.hasNextInt())
-                    count = scanner.nextInt();
-//                if (scanner.hasNextDouble())
-                    price = scanner.nextDouble();
+                googsName = scanner.next();
+                count = scanner.nextInt();
+                price = scanner.nextDouble();
             } catch (Exception e) {
                 System.out.println("Ошибочка вышла!");
                 good = false;
                 break;
             }
-//            System.out.println(googsName + " / " + count + " / " + price);
             oList.add(new OrderItem(googsName, count, price));
             sumOrder += count * price;
         }
@@ -96,23 +99,20 @@ public class OrderProcessor {
 //        System.out.println(ld + " ("+start+" = "+finish+")");
         if (lastDate.compareTo(start) >= 0 && lastDate.compareTo(finish) <= 0)
             return true;
-        else
-            return false;
+        return false;
     }
     //  -----------------------------------
     //  преобразование строки в дату, 2020-04-03 - дата из атрибутов файла
     public LocalDate checkLastDate(String last){
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        LocalDate localDate = LocalDate.from(dtf.parse(last)); //  преобразовать строку в дату
-        return localDate;
+        return LocalDate.from(dtf.parse(last));
     }
     public LocalDateTime checkLastTime(String last){
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime localTime = LocalDateTime.from(dtf.parse(last)); //  преобразовать строку в дату
-        return localTime;
+        return LocalDateTime.from(dtf.parse(last));
     }
     //  -----------------------------------
-    public static List<Path> createList(String startPath, String pattern) throws IOException {
+    public static List<Path> createList(String startPath, String pattern){
         final Path dir = Paths.get(startPath);
         List<Path> fList = new ArrayList<>();
         try {
@@ -175,7 +175,7 @@ public class OrderProcessor {
 
     return null;
     }
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         OrderProcessor ord1 = new OrderProcessor("C:/Projects/Academy/Java1");
         LocalDate data1 = LocalDate.of(2020, 4, 13);
         LocalDate data2 = LocalDate.of(2020, 4, 19);
