5a936978d32b333eb7366dca70a0b15df0b90a3e
sidnet1964 <sidnet@mail.ru>
2020-04-27 18:17:19 +0300

    H19 Домашнее задание

--- src/ru/progwards/java1/lessons/N19/RamCompiler.java
+++ src/ru/progwards/java1/lessons/N19/RamCompiler.java
@@ -355,7 +355,7 @@ public class RamCompiler {
         //      с учетом перехода по меткам
         //  - структура данных для моделирования памяти
 
-        RamCompiler pr1 = new RamCompiler("C:/Projects/Academy/Java1/reverse.txt");
+        RamCompiler pr1 = new RamCompiler("C:/Projects/Academy/Java1/factorial.txt");
 
         System.out.println("Переменная errorCom = " + pr1.isErrorCom());
         //  есть вариант определить в конструкторе каталог, а имена файлов сделать стандартными
