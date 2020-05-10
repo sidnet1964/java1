5704f0cda681829af792e04745bd6febde7fa3bc
sidnet1964 <sidnet@mail.ru>
2020-05-11 01:58:20 +0300

    J2_3 Домашнее задание

--- src/ru/progwards/java2/lessons/recursion/GoodsWithLambda.java
+++ src/ru/progwards/java2/lessons/recursion/GoodsWithLambda.java
@@ -8,98 +8,71 @@ import java.util.stream.Collectors;
 
 public class GoodsWithLambda {
     static List<Goods> lst;
-    //  1.3 реализовать метод
+    //  1.3 реализовать метод setGoods
     static void setGoods(List<Goods> list) {
-        lst = new ArrayList<>(list);
-        lst.forEach(element -> System.out.println("element = " + element));
-        System.out.println();
+        lst = new ArrayList<>(list);    // заполнение списка товаров
     }
-    //  вернуть список, отсортированный по наименованию
+    //  1.4 вернуть список, отсортированный по наименованию
     static List<Goods> sortByName() {
         lst.sort((a, b) -> a.name.compareTo(b.name));
-        lst.forEach(element -> System.out.println(element));
-        return null;
+        return lst;
     }
-    //  вернуть список, отсортированный по артикулу, без учета регистра
+    //  1.5 вернуть список, отсортированный по артикулу, без учета регистра
     static List<Goods> sortByNumber(){
         lst.sort((a, b) -> a.number.toUpperCase().compareTo(b.number.toUpperCase()));
-        lst.forEach(element -> System.out.println(element));
-        return null;
+        return lst;
     }
     //  -----------------------------------------
-    //   вернуть список, отсортированный по первым 3-м символам артикула, без учета регистра
+    //   1.6 вернуть список, отсортированный по первым 3-м символам артикула, без учета регистра
     static List<Goods> sortByPartNumber(){
         lst.sort((a, b) -> a.number.substring(0,3).toUpperCase().compareTo(b.number.substring(0,3).toUpperCase()));
-        lst.forEach(element -> System.out.println(element));
-        return null;
+        return lst;
     }
     //  -----------------------------------------
-    //  вернуть список, отсортированный по количеству, а для одинакового количества,
+    //  1.7 вернуть список, отсортированный по количеству, а для одинакового количества,
     //  по артикулу, без учета регистра
     static List<Goods> sortByAvailabilityAndNumber(){
 //        //  проверить гипотезу сохранения порядка, вариант рабочий - отключен для поиска других
 //        lst.sort((a, b) -> a.number.substring(0,3).toUpperCase().compareTo(b.number.substring(0,3).toUpperCase()));
 //        lst.sort((a, b) -> Integer.compare(a.available, b.available));
-//        lst.forEach(System.out::println);
 
-        //        list.sort(Comparator.comparing(a -> a.name)); //  проверить вариант
-//      вариант ниже не работает, хотя каждый отдельный sort правильный
-//        lst.sort(Comparator.comparing(a -> a.number.toUpperCase())).sort(Comparator.comparing(a -> a.available));
-// формирование списка по критерию через поток
-//        List<Integer> filtered = lst.stream().sorted().collect(Collectors.toList());
-    // вывод на консоль
-//        System.out.println(filtered);
         List<Goods> sorted = lst.stream().sorted(Comparator.comparing(a -> a.number.toUpperCase())).sorted(Comparator.comparing(a -> a.available)).collect(Collectors.toList());
-        sorted.forEach(System.out::println);
-
-        return null;
+        return sorted;
     }
     //  -----------------------------------------
-    //  вернуть список, с товаром, который будет просрочен после указанной даты, отсортированный по дате годности
+    //  1.8 вернуть список, с товаром, который будет просрочен после указанной даты,
+    //  отсортированный по дате годности
     static List<Goods> expiredAfter(Instant date){
         // формирование списка по критерию через поток
         List<Goods> filtered = lst.stream().filter(x -> date.compareTo(x.expired)>0).sorted(Comparator.comparing(x -> x.expired)).collect(Collectors.toList());
-        filtered.forEach(System.out::println);
-//        Instant goden = lst.get(1).expired;
-//        System.out.println(date);
-//        System.out.println(date.compareTo(goden));
-
-        return null;
+        return filtered;
     }
-    //  вернуть список, с товаром, количество на складе которого меньше указанного
+    //  1.9 вернуть список, с товаром, количество на складе которого меньше указанного
     static List<Goods> сountLess(int count){
         List<Goods> filtered = lst.stream().filter(x -> x.available < count).collect(Collectors.toList());
-        filtered.forEach(System.out::println);
-
-        return null;
+        return filtered;
     }
-    //  вернуть список, с товаром, количество на складе которого
+    //  1.10 вернуть список, с товаром, количество на складе которого
     //  больше count1 и меньше count2
     static List<Goods> сountBetween(int count1, int count2){
         List<Goods> filtered = lst.stream().filter(x -> x.available > count1).filter(x -> x.available < count2).collect(Collectors.toList());
-        filtered.forEach(System.out::println);
-        return null;
+        return filtered;
     }
 
     public static void main(String[] args) {
-//        List<Goods> list = new ArrayList<>(List.of(
-//                new Goods("Икра", "111", 5, 120),
-//                new Goods("Лечо", "222", 6, 130),
-//                new Goods("Перец", "333", 7, 125),
-//                new Goods("Томаты", "444", 6, 110),
-//                new Goods("Соль", "555", 10, 12)));
         setGoods(List.of(
-                new Goods("Икра", "a1166", 5, 120, Instant.parse("2020-05-06T10:02:32.959644400Z")),
+                new Goods("Икра", "a1166", 5, 120, Instant.parse("2020-05-16T10:02:32.959644400Z")),
                 new Goods("Лечо", "e222", 6, 130, Instant.parse("2020-05-10T10:02:32.959644400Z")),
-                new Goods("Перец", "d333", 7, 125, Instant.parse("2020-05-05T10:02:32.959644400Z")),
+                new Goods("Перец", "d333", 7, 125, Instant.parse("2020-05-15T10:02:32.959644400Z")),
                 new Goods("Томаты", "C444", 6, 110, Instant.parse("2020-05-10T10:02:32.959644400Z")),
-                new Goods("Соль", "a11555", 6, 12, Instant.parse("2020-05-04T10:02:32.959644400Z"))));
-//        System.out.println(sortByName());
-//        System.out.println(sortByNumber());
-//        System.out.println(sortByPartNumber());
-//        System.out.println(sortByAvailabilityAndNumber());
-//        expiredAfter(Instant.now());
-//        System.out.println(сountLess(6));
-        сountBetween(15, 17);
+                new Goods("Соль", "a11555", 6, 12, Instant.parse("2020-05-14T10:02:32.959644400Z"))));
+
+        System.out.println(sortByName());                   //  1.4
+        System.out.println(sortByNumber());                 //  1.5
+        System.out.println(sortByPartNumber());             //  1.6
+        System.out.println(sortByAvailabilityAndNumber());  //  1.7
+        System.out.println(expiredAfter(Instant.now()));    //  1.8
+        System.out.println(сountLess(6));             //  1.9
+        System.out.println(сountBetween(5, 7));           //  1.10
     }
 }
--- src/ru/progwards/java2/lessons/recursion/HanoiTower.java
+++ src/ru/progwards/java2/lessons/recursion/HanoiTower.java
@@ -3,55 +3,30 @@ package ru.progwards.java2.lessons.recursion;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 
+import static java.lang.String.format;
+
+//@SuppressWarnings("ALL")
 public class HanoiTower {
     final static int p3 = 3;  //  для чистоты кода
     int size;
     int pos;
-    ArrayDeque<Integer>[] pin;  //  стек на штыре
+    ArrayDeque<Integer>[] pin;  //  массив стеков на штырях
+    boolean setTrace;
+
     //  --------------------------------
     //  конструктор
     public HanoiTower(int size, int pos) {
+        setTrace = false;
         this.size = size;
         this.pos = pos;
         ArrayDeque<Integer> pin1 = new ArrayDeque<Integer>();
         ArrayDeque<Integer> pin2 = new ArrayDeque<Integer>();
         ArrayDeque<Integer> pin3 = new ArrayDeque<Integer>();
-//        int[] pin = new int[3]; //  массив штырей
         pin = new ArrayDeque[]{pin1, pin2, pin3}; //  массив штырей
+//        pin = new ArrayDeque<Integer>()[3]; //  массив штырей
         for (int i = size; i>0; i--)    //  внизу кольцо size, вверху - 1
             pin[pos].addFirst(i);
-        System.out.println(pin[pos]);
-//  Stack<MyClass>[] arr = new Stack<MyClass>[10];
-//  Stack<Object>[] arr = (Stack<Object>[]) new Stack<?>[10];
-//        input       = new ArrayDeque<>();
-    }
-    //  --------------------------------
-    //  --------- перестановки с from на to
-    //  переносит башню со штыря from на штырь to
-    public void moveX(int from, int to){
-        int round = 0;  //  кольцо для переноса
-        int rFr = 0; int rTo = 0;
-        boolean bFr = true;
-        boolean bTo = false;
-        if (pin[from].peekFirst() == null)
-            bFr = false;
-        else {
-            rFr = pin[from].peekFirst();  //  посмотреть, что лежит сверху
-            if (pin[to].peekFirst() == null) {
-                bTo = true;
-                round = pin[from].removeFirst();    //  снать кольцо
-                pin[to].addFirst(round);            //  положить кольцо
-            }
-            else {
-                rTo = pin[to].peekFirst();  //  посмотреть, что лежит сверху
-                if (rFr < rTo){
-                    round = pin[from].removeFirst();    //  снать кольцо
-                    pin[to].addFirst(round);            //  положить кольцо
-                }
-            }
-        }
-        System.out.println(pin[from].peekFirst() + " -> " + pin[to].peekFirst() );
-//        System.out.println(rFr + " -> " + rTo );
+//        System.out.println(pin[pos]);
     }
     //  --------------------------------
     void print() {
@@ -59,59 +34,41 @@ public class HanoiTower {
         Integer[][] ar2 = new Integer[p3][];
         for (int i = 0; i<p3; i++) {
             ArrayDeque<Integer> pinX = pin[i];
-//            System.out.println(pinX.size());
-            Integer[] arrInt = pinX.toArray(new Integer[pinX.size()]);  //  зубчатый массив
-//            Integer[] arrInt = pinX.toArray(new Integer[size]); //  для прямоугольного массива, пустые - null
+            Integer[] arrInt = pinX.toArray(new Integer[0]);  //  зубчатый массив
             ar2[i] = arrInt;
         }
-        System.out.println(Arrays.deepToString(ar2));
         //  цикл по горизонталям
         for (int i = 0; i<size; i++){
-            String line = "";
+            StringBuilder line = new StringBuilder();
             //  цикл по штырям (всего их 3)
             for (int j = 0; j<p3; j++) {
                 int shift = size - ar2[j].length;   //  пропуск верхних ячеек
                 if (shift > i)
-                    line = line + "  |  " + "_";
+                    line.append("  I  ").append(" ");
                 else
-                    line = line + "<00" + ar2[j][i-shift] + ">" + "_";
+                    line.append(format("<%03d>", ar2[j][i - shift])).append(" ");
             }
             System.out.println(line.substring(0,line.length()-1));
         }
-        System.out.println("=====+=====+=====");
+        System.out.println("=================\n");
     }
     //  включает отладочную печать состояния игрового поля после каждого шага алгоритма
     //  (метода moveX). В результате все промежуточные ходы должны быть отображены
     void setTrace(boolean on){
-
-    }
-    //  --------------------------------
-    public static void main(String[] args) {
-        HanoiTower tower = new HanoiTower(5,0);
-        tower.print();
-        tower.move(0,2);
-        tower.print();
-    }
-    public void move(int from, int to){
-        moveStack(from, to, this.size);
+        setTrace = on;   //  установить состояния
     }
     //  --------------------------------
     void moveStack(int from, int to, int count) {
-        int othe = -1;
-//        System.out.println("+++=" + count + "=->=" + from + "=" + othe + "=" + to);
         if (count > 0){
-            othe = getIndex(from, to);
+            int othe = getIndex(from, to);
             moveStack(from, othe,count-1); // перенести башню из n−1 диска на 2-й штырь
-            ArrayDeque from_X = this.pin[from];
-            ArrayDeque to_X = this.pin[to];
-            int top = (int) from_X.pollFirst();
+            ArrayDeque<Integer> from_X = this.pin[from];
+            ArrayDeque<Integer> to_X = this.pin[to];
+            int top = from_X.pollFirst();  // переносим верхний диск
             to_X.addFirst(top);
-//            int biggest = from.pop();
-//            System.out.println(">>>=" + biggest);
-//            to.push(biggest); // переносим самый большой диск на 3-й штырь
-            //  отладочная печать
-            this.print();
-            moveStack(othe,to,count-1); // перенеси башню из n−1 диска на 3-й штырь
+            if (setTrace)
+                this.print();
+            moveStack(othe,to,count-1); // перенеси башню из n−1 диска на 2-й штырь
         }
 //        System.out.println("---=" + count + "=->=" + from + "=" + othe + "=" + to);
     }
@@ -136,5 +93,16 @@ public class HanoiTower {
 //    Assert(False,'wrong indeces');
         }
         return res;
+    }
+    public void move(int from, int to){
+        moveStack(from, to, this.size);
+    }
+    //  --------------------------------
+    public static void main(String[] args) {
+        HanoiTower tower = new HanoiTower(4,0);
+        tower.print();
+        tower.setTrace(true);
+        tower.move(0,2);
+        tower.print();
     }
 }
