7e95df386d4403cdafb667ba24e20e221c2f0f53
sidnet1964 <sidnet@mail.ru>
2020-06-10 19:55:11 +0300

    J2_6 Домашнее задание

--- src/ru/progwards/java2/lessons/gc/Heap.java
+++ src/ru/progwards/java2/lessons/gc/Heap.java
@@ -68,45 +68,47 @@ public class Heap {
         }
         throw new InvalidPointerException("Неверный адрес участка памяти", ptr);
     }
+    //  ------------------------------------------------------------------------
     //  осуществляет дефрагментацию кучи
     public void defrag(){
         if (inputList.size() <= 1)
             return; //  пустой список или состоит из одного элемента
         //  отсортировать inputList
         inputList.sort((a, b) -> Integer.compare(a.ptr, b.ptr));
+//        System.out.println(inputList);
+//        System.out.println(outputList);
+//        System.out.println(this.toString());
+
         ArrayDeque<Integer> input;  //  коллекция для смежных блоков
 //        while (true) {  //  выход по внутреннему условию
-        for (int i = 0; i < inputList.size(); i++) {    //  проверить список из одного элемента
+        for (int i = 0; i < inputList.size()-1; i++) {    //   i++    проверить список из одного элемента
             input = new ArrayDeque<>();
             input.addLast(i);
+//            System.out.println("Нач i = " + i + " input = " + input);
             //  чтобы объединить все смежые блоки необходимо
             int prevPtr = inputList.get(i).ptr;     //  начало первого блока
             int prevSize = inputList.get(i).size;   //  размер первого блока
             for (int j = i+1; j < inputList.size(); j++) {    //  проверить список из одного элемента
                 i++;    //  работа с внешним счетчиком
                 Allock block = inputList.get(j);
-//                System.out.println("j = " + j + " prevPtr = " + prevPtr + " prevSize = " + prevSize);
+//                System.out.println("Вхо i = " + i + ", j = " + j + " Ptr = " + prevPtr + " Size = " + prevSize);
                 if (prevPtr + prevSize == block.ptr) {
                     input.addLast(j);
 //                    System.out.println("Общий размер = " + (prevSize + block.size));
                     prevSize += block.size; //  увеличить размер блока для проверки
-                    //  увеличить предыдущий блок на текущий size
-//                    block.size += prevSize;
-//                    block.ptr = prevPtr;
-//                    memFill(block.ptr, block.size, (byte) (-1 * (sFill++)));
-//                    inputList.set(j - 1, block);  //  записать все в предыдущий блок
-//                    inputList.remove(j);        //  удалить текущий блок
                 }
                 else {
-                    break;
+                    break;  //  выход из цикла по j
                 }
-                System.out.println("j = " + j + " input = " + input);
+//                System.out.println("Вых i = " + i + " , j = " + j + " input = " + input);
             }
             //  выход из цикла
             //  удалить лишние блоки из списка
+//            i -= input.size();    //  отмотать счетчик
             while (input.size() > 1) {
                 int indDel = input.removeLast();
                 inputList.remove(indDel);
+                i--;    //  отмотать счетчик
             }
             int indDel = input.removeLast();
             Allock block = inputList.get(indDel);
@@ -114,12 +116,75 @@ public class Heap {
             block.ptr = prevPtr;
             memFill(block.ptr, block.size, (byte) (-1 * (sFill++)));
             inputList.set(indDel, block);  //  записать все в предыдущий блок
-            System.out.println("i = " + i + " input = " + input);
+            i--;    //  отмотать счетчик
+//            System.out.println("Кон i = " + i + " input = " + input);
+//            System.out.println();
+//            System.out.println(inputList);
+//            System.out.println(outputList);
+//            System.out.println(this.toString());
         }
     }
+    //  ------------------------------------------------------------------------
     //  компактизация кучи - перенос всех занятых блоков в начало хипа
     public void compact(){
-        
+        if (outputList.size() == 0)
+            return; //  пустой список, нечего переносить
+        //  отсортировать inputList
+        inputList.sort((a, b) -> Integer.compare(a.ptr, b.ptr));
+        //  отсортировать outputList
+        outputList.sort((a, b) -> Integer.compare(a.ptr, b.ptr));
+        for (int i = 0; i < outputList.size(); i++) {
+            Allock oBlock = outputList.get(i);
+            for (int j = 0; j < inputList.size(); j++) {
+                Allock iBlock = inputList.get(j);
+                if (oBlock.ptr < iBlock.ptr)
+                    break;
+                else {
+                    System.out.println("Вхо i = " + i + ", j = " + j + "/" + oBlock + "/" + iBlock);
+                    if (oBlock.size <= iBlock.size) {
+                        //  начало переноса
+                        for (int k = 0; k < oBlock.size; k++) {
+                            bytes[iBlock.ptr + k] = bytes[oBlock.ptr + k];
+                            bytes[oBlock.ptr + k] = (byte) -bytes[oBlock.ptr + k]; //  "очистка" памяти
+                        }
+                        if ((oBlock.size == iBlock.size)) {
+                            //  блоки равны по размеру, обновить оба адреса
+                            int iPtr = iBlock.ptr;
+                            iBlock.ptr = oBlock.size;   //  заменить адрес блока
+                            oBlock.ptr = iPtr;          //  заменить адрес блока
+                        } else {
+                            //  обновить элементы списков
+                            int oPtr = oBlock.ptr;      //  запомнить одес занятого блока
+                            int oSize = iBlock.size - oBlock.size;  //  разница размеров
+                            oBlock.ptr = iBlock.ptr;    //  заменить адрес блока
+                            iBlock.ptr += oBlock.size;  //  заменить адрес блока
+                            iBlock.size -= oBlock.size; //  уменьшить размер свободного блока
+                            inputList.add(new Allock(oPtr, oSize)); //  новый свободный блок на разность
+                        }
+                        inputList.set(j, iBlock);
+                        outputList.set(i, oBlock);
+                        //  отсортировать inputList
+                        inputList.sort((a, b) -> Integer.compare(a.ptr, b.ptr));
+                        //  отсортировать outputList
+//                    outputList.sort((a, b) -> Integer.compare(a.ptr, b.ptr));
+
+                    }
+//                    if (iBlock.size == 0)
+//                        //  удалить пустой блок
+//                        inputList.remove(j);
+//                    else
+//                    {
+//                        inputList.set(j,iBlock);
+//                        outputList.set(i,oBlock);
+//                    }
+                    break;  //  выход из цикла по j
+                }
+            }
+        System.out.println("Кон i = " + i);
+        System.out.println(inputList);
+        System.out.println(outputList);
+        System.out.println(this.toString());
+        }
     }
 
     @Override
@@ -140,9 +205,8 @@ public class Heap {
 
         @Override
         public String toString() {
-            return "{" +
-                    "ptr=" + ptr +
-                    ", size=" + size +
+            return "{" + ptr +
+                    ", " + size +
                     '}';
         }
     }
@@ -165,19 +229,30 @@ public class Heap {
         System.out.println(extMemory.toString());
         extMemory.free(2);
         extMemory.free(4);
-        extMemory.free(6);
+//        extMemory.free(6);
 
         extMemory.free(10);
         extMemory.free(12);
         extMemory.free(14);
 
-        System.out.println(extMemory.inputList);
-        System.out.println(extMemory.outputList);
-        System.out.println(extMemory.toString());
+//        System.out.println(extMemory.inputList);
+//        System.out.println(extMemory.outputList);
+//        System.out.println(extMemory.toString());
+
         extMemory.defrag();
+
+        System.out.println();
         System.out.println(extMemory.inputList);
         System.out.println(extMemory.outputList);
         System.out.println(extMemory.toString());
+
+        extMemory.compact();
+
+        System.out.println();
+        System.out.println(extMemory.inputList);
+        System.out.println(extMemory.outputList);
+        System.out.println(extMemory.toString());
+
     }
 }
 //[{ptr=16, size=32}, {ptr=48, size=64}, {ptr=240, size=784}]
